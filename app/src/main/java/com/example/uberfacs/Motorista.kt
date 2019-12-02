e6104f27e74242e04eddb5220039558e6f784f16
gabrielvbrandao <50991850+gabrielvbrandao@users.noreply.github.com>
2019-12-02 15:39:43 -0300

    Create Motorista.kt

---
+++ app/src/main/java/com/example/uberfacs/Motorista.kt
@@ -0,0 +1,10 @@
+class Motorista (
+  internal var nome: String,
+  internal var telefone: Int,
+  internal var cursomotorista: String,
+  internal var txtcampusmotorista: String,
+  internal var txtenderecomotorista: String,
+  internal var txtemailmotorista: String,
+  internal var placa: String,
+  internal var modelo: String)
+{ }
