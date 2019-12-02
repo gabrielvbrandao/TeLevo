0f097313b56d726a64f8316a776c72ba62f061d8
gabrielvbrandao <50991850+gabrielvbrandao@users.noreply.github.com>
2019-12-02 15:33:15 -0300

    Create Usuario.kt

---
+++ app/src/main/java/com/example/uberfacs/Usuario.kt
@@ -0,0 +1,8 @@
+class Usuario (
+  internal var nome:String,
+  internal var telefone: Int,
+  internal var campus: String,
+  internal var endereco: String,
+  internal var email: String,
+  internal var senha: String)
+{ }
